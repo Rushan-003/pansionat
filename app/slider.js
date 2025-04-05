1c9c3f95e067fd044a32fa705c3a91c34ff9f764
Rushan <xamidullin.rushan@gmail.com>
2025-04-05 20:52:45 +0300

    0.3

---
+++ app/slider.js
@@ -0,0 +1,12 @@
+let date = Date();
+let controlLeft = document.querySelector("button");
+controlLeft.onclick = () => {
+    console.log("Нажато!");
+}
+
+let button_1 = document.getElementById('buttom_1');
+
+function click() {
+
+}
+ 
